bca50215e70df571345b976fba38cbc8fc4fc3af
xxx <humengmingxx@gmail.com>
2022-07-22 00:01:45 +0800

    feat：boolean

---
+++ halo/boolean.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	xxx := true
+	yyy := false
+
+	fmt.Println(xxx)
+	fmt.Println(yyy)
+	fmt.Println("true && false = ", xxx && yyy)
+	fmt.Println("true || false = ", xxx || yyy)
+}
