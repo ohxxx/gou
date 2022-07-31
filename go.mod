0cf875acf6dcfe9b355c0f4fd93fc4cb718f9957
xxx <humengmingxx@gmail.com>
2022-07-31 20:45:11 +0800

    feat：disable

--- go.mod
+++
@@ -1,3 +0,0 @@
-module packet_book
-
-go 1.18
--- halo/packet.go
+++ halo/packet.go
@@ -3,15 +3,6 @@ package main
 // error：package packet_book is not in GOROOT
 // 处理：go mod init pkg-name
 
-import (
-	// 别名：book
-	book "packet_book/halo/packet_book"
-)
-
-// 包的匿名导入
-// 包的匿名导入是一种特殊的导入，它可以被用来导入包中的所有内容，包括子包。
-import . "fmt"
-
 //import _ "fmt"
 
 func init() {
@@ -19,8 +10,8 @@ func init() {
 }
 
 func main() {
-	bookName := "halo go"
-	bookPrice := float64(100)
-	bookInfo, _ := book.ShowBookInfo(bookName, bookPrice)
-	Println(bookInfo)
+	//bookName := "halo go"
+	//bookPrice := float64(100)
+	//bookInfo, _ := book.ShowBookInfo(bookName, bookPrice)
+	//Println(bookInfo)
 }
