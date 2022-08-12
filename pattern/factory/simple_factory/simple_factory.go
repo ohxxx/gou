8cd60e910b202d1da04f5212a9d262dde9df1cdc
xxx <humengmingxx@gmail.com>
2022-08-12 16:38:29 +0800

    feat：done

--- pattern/factory/simple_factory/simple_factory.go
+++ pattern/factory/simple_factory/simple_factory.go
@@ -1,1 +1,32 @@
 package simple_factory
+
+// 【创建型】
+// 简单工厂：定义一个用于创建对象的接口，让子类决定实例化哪一个类。
+// Go 本身没有构造函数，一般采用 NewName 方法来创建 对象/接口，当返回值是接口的时候其实就是简单工厂模式
+
+type IRuleConfigParser interface {
+	Parse(data []byte)
+}
+
+type JsonRuleConfigParser struct{}
+
+type YamlRuleConfigParser struct{}
+
+func (J JsonRuleConfigParser) Parse(data []byte) {
+	panic("implement xxx")
+}
+
+func (Y YamlRuleConfigParser) Parse(data []byte) {
+	panic("implement xxx")
+}
+
+func RuleConfigParser(name string) IRuleConfigParser {
+	switch name {
+	case "json":
+		return JsonRuleConfigParser{}
+	case "yaml":
+		return YamlRuleConfigParser{}
+	default:
+		return nil
+	}
+}
--- pattern/factory/simple_factory/simple_factory_test.go
+++ pattern/factory/simple_factory/simple_factory_test.go
@@ -1,1 +1,37 @@
 package simple_factory
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRuleConfigParser(t *testing.T) {
+	type args struct {
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want IRuleConfigParser
+	}{
+		{
+			name: "json",
+			args: args{t: "json"},
+			want: JsonRuleConfigParser{},
+		},
+		{
+			name: "yaml",
+			args: args{t: "yaml"},
+			want: YamlRuleConfigParser{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := RuleConfigParser(tt.args.t); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("RuleConfigParser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
