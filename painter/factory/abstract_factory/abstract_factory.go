3e20fb6d48881a48ca8698fed3d08f611f9ef024
xxx <humengmingxx@gmail.com>
2022-08-15 13:06:08 +0800

    feat：done

--- painter/factory/abstract_factory/abstract_factory.go
+++ painter/factory/abstract_factory/abstract_factory.go
@@ -1,1 +1,37 @@
 package abstract_factory
+
+type IRuleConfigParser interface {
+	Parse(data []byte)
+}
+
+type jsonRuleConfigParser struct{}
+
+func (j jsonRuleConfigParser) Parse(data []byte) {
+	panic("implement me")
+}
+
+type ISystemConfigParser interface {
+	ParseSystem(data []byte)
+}
+
+type jsonSystemConfigParser struct{}
+
+func (j jsonSystemConfigParser) ParseSystem(data []byte) {
+	panic("implement xxx")
+}
+
+// IConfigParserFactory 工厂方法接口
+type IConfigParserFactory interface {
+	CreateRuleParser() IRuleConfigParser
+	CreateSystemParser() ISystemConfigParser
+}
+
+type jsonConfigParserFactory struct{}
+
+func (j jsonConfigParserFactory) CreateRuleParser() IRuleConfigParser {
+	return jsonRuleConfigParser{}
+}
+
+func (j jsonConfigParserFactory) CreateSystemParser() ISystemConfigParser {
+	return jsonSystemConfigParser{}
+}
--- painter/factory/abstract_factory/abstract_factory_test.go
+++ painter/factory/abstract_factory/abstract_factory_test.go
@@ -1,1 +1,48 @@
 package abstract_factory
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestJsonConfigParserFactoryCreateRuleParser(t *testing.T) {
+	tests := []struct {
+		name string
+		want IRuleConfigParser
+	}{
+		{
+			name: "json",
+			want: jsonRuleConfigParser{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			j := jsonConfigParserFactory{}
+			if got := j.CreateRuleParser(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CreateRuleParser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestJsonConfigParserFactoryCreateSystemParser(t *testing.T) {
+	tests := []struct {
+		name string
+		want ISystemConfigParser
+	}{
+		{
+			name: "json",
+			want: jsonSystemConfigParser{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			j := jsonConfigParserFactory{}
+			if got := j.CreateSystemParser(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CreateSystemParser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
